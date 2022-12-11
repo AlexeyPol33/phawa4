7a55c415b9b35c4c998b6514591a6d5744c055b1
Alexey Poltavskiy <79776177342@yandex.ru>
2022-12-11 14:39:38 +0300

    task 1 err

---
+++ task1.py
@@ -0,0 +1,46 @@
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.list_of_list = list_of_list
+        print(self.list_of_list)
+
+
+    def __iter__(self):
+        print('начало итерации')
+        
+        self.items = self.list_of_list
+        if self.items:
+            self.items = self.list_of_list.pop(0)
+        
+        return self
+
+    def __next__(self):
+        if not self.list_of_list and not self.items:
+            raise StopIteration
+        if not self.items:
+            self.items = self.list_of_list.pop(0)
+        item = self.items.pop(0)
+        print(item)
+        return item
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
